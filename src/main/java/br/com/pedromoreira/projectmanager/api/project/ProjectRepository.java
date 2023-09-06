b81bb50e9deed3ce48e30d7b3508af2610fc4103
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-06 10:07:35 -0300

    feat: adicionado função para busca pelo nome na interface de Project

--- src/main/java/br/com/pedromoreira/projectmanager/api/project/ProjectRepository.java
+++ src/main/java/br/com/pedromoreira/projectmanager/api/project/ProjectRepository.java
@@ -1,7 +1,12 @@
 package br.com.pedromoreira.projectmanager.api.project;
 
+import br.com.pedromoreira.projectmanager.api.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-
+    @Query(value = "SELECT p FROM projects p WHERE p.nome LIKE %?1%")
+    List<Project> findByName(String nome);
 }
