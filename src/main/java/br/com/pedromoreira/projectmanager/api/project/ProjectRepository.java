07278ab021fe92c12074e12f3fd6f1057b5036a9
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-06 00:41:33 -0300

    feat: adicionado interface para Project

---
+++ src/main/java/br/com/pedromoreira/projectmanager/api/project/ProjectRepository.java
@@ -0,0 +1,7 @@
+package br.com.pedromoreira.projectmanager.api.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
