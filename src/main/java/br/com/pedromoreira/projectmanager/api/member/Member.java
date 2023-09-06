7d02f474fc5c181d88b8eb5c0b72b5d6297b2125
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-05 23:41:48 -0300

    feat: adicionado classe principal: Member

---
+++ src/main/java/br/com/pedromoreira/projectmanager/api/member/Member.java
@@ -0,0 +1,31 @@
+package br.com.pedromoreira.projectmanager.api.member;
+
+import br.com.pedromoreira.projectmanager.api.project.Project;
+import br.com.pedromoreira.projectmanager.api.task.Task;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "members")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String nome;
+    private String email;
+    private String cargo;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", referencedColumnName = "id", nullable = true)
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", referencedColumnName = "id", nullable = true)
+    private Task task;
+}
