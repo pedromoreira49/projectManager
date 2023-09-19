e2da41a08bd072ed9f9c8fcd652bd26f2d708dc6
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-18 21:40:25 -0300

    refact: adicionado atributo members para listar membros vinculados ao projeto

--- src/main/java/br/com/pedromoreira/projectmanager/api/project/ProjectDTO.java
+++ src/main/java/br/com/pedromoreira/projectmanager/api/project/ProjectDTO.java
@@ -1,9 +1,12 @@
 package br.com.pedromoreira.projectmanager.api.project;
 
+import br.com.pedromoreira.projectmanager.api.member.Member;
+import br.com.pedromoreira.projectmanager.api.member.MemberDTO;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class ProjectDTO {
@@ -12,6 +15,7 @@ public class ProjectDTO {
     private String descricao;
     private Date dataDeInicio;
     private Date dataDeConclusao;
+    private List<MemberDTO> members;
 
     public static ProjectDTO create(Project p){
         ModelMapper modelMapper = new ModelMapper();
