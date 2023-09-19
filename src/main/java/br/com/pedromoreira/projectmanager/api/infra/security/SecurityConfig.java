4d3506785f45d074069a5e685b4148d382ade5ea
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-18 21:25:04 -0300

    refac: alterado lógica para configuração de segurança com jwt

--- src/main/java/br/com/pedromoreira/projectmanager/api/infra/security/SecurityConfig.java
+++ src/main/java/br/com/pedromoreira/projectmanager/api/infra/security/SecurityConfig.java
@@ -1,15 +1,20 @@
 package br.com.pedromoreira.projectmanager.api.infra.security;
 
+import br.com.pedromoreira.projectmanager.api.infra.security.jwt.JwtAuthenticationFilter;
+import br.com.pedromoreira.projectmanager.api.infra.security.jwt.JwtAuthorizationFilter;
+import br.com.pedromoreira.projectmanager.api.infra.security.jwt.handler.AccessDeniedHandler;
+import br.com.pedromoreira.projectmanager.api.infra.security.jwt.handler.UnauthorizedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,9 +23,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
     @Autowired
     @Qualifier("userDetailService")
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private UnauthorizedHandler unauthorizedHandler;
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -32,16 +43,27 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        final AuthenticationManager authManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+
         http
-                .csrf().disable()
-                .authorizeHttpRequests()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login", "/api/v1/users/register").permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic(Customizer.withDefaults())
-                .userDetailsService(userDetailsService);
+                .csrf().disable()
+                .addFilter(new JwtAuthenticationFilter(authManager))
+                .addFilter(new JwtAuthorizationFilter(authManager, userDetailsService))
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
+
 }
