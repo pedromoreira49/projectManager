02b8b420f226cffe648b4420c11cbc61f24c988b
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-18 21:21:54 -0300

    feat: adicionado classe para tratamento de exceção para acesso negado

---
+++ src/main/java/br/com/pedromoreira/projectmanager/api/infra/security/jwt/handler/AccessDeniedHandler.java
@@ -0,0 +1,31 @@
+package br.com.pedromoreira.projectmanager.api.infra.security.jwt.handler;
+
+import br.com.pedromoreira.projectmanager.api.infra.security.jwt.ServletUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AccessDeniedHandler implements org.springframework.security.web.access.AccessDeniedHandler{
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException exc) throws IOException {
+
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+
+            String json = ServletUtil.getJson("error", "Acesso negado.");
+            ServletUtil.write(response, HttpStatus.FORBIDDEN, json);
+        }
+    }
+}
