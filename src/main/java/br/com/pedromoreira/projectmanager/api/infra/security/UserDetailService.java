8742a8c520b27d3e6e69d84f061d32606d4ca05b
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-18 21:25:49 -0300

    refac: alterado estrutura da classe UserDetailService para utilização de jwt

--- src/main/java/br/com/pedromoreira/projectmanager/api/infra/security/UserDetailService.java
+++ src/main/java/br/com/pedromoreira/projectmanager/api/infra/security/UserDetailService.java
@@ -1,22 +1,20 @@
 package br.com.pedromoreira.projectmanager.api.infra.security;
 
-import org.springframework.security.core.userdetails.User;
+import br.com.pedromoreira.projectmanager.api.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service(value = "userDetailService")
 public class UserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRep;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        if(username.equals("user")){
-            return User.withUsername(username).password(encoder.encode("123")).roles("USER").build();
-        }else if(username.equals("admin")){
-            return User.withUsername(username).password(encoder.encode("123")).roles("USER", "ADMIN").build();
-        }
-        throw new UsernameNotFoundException("Usuario inexistente.");
+        return userRep.findByLogin(username);
     }
+
 }
