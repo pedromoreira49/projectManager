21c254c0411ffdb93f7dd0c42d395168888fefd3
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-09-18 21:14:55 -0300

    feat: adicionado repository para Role

---
+++ src/main/java/br/com/pedromoreira/projectmanager/api/user/RoleRepository.java
@@ -0,0 +1,6 @@
+package br.com.pedromoreira.projectmanager.api.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
